54e57eb58ff7ac7783b9e4c3ddcd56b7656390c2
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2021-10-10 06:21:40 +0530

    Add files via upload

---
+++ php_veriables.php
@@ -0,0 +1,21 @@
+<?php 
+	echo "Hello World <br>";
+
+	$str = "Welcome to JEHANKANY <br>";
+	echo $str;
+
+	$int = 2;
+	$float = 0.2;
+	
+	echo $int ."<br>". $float ."<br>";
+
+	echo "Basic Cal : <br>";
+	$n1 = 10;
+	$n2 = 20;
+
+	echo $n1 + $n2 ."<br>";
+	echo $n1 - $n2 ."<br>";
+	echo $n1 * $n2 ."<br>";
+	echo $n1 / $n2 ."<br>";
+
+?>
